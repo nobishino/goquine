f6765c91d8e2fb77a7e68a219e4e0c0bf34a0dfa
cia-rana <kiwamura0314@gmail.com>
2022-11-22 00:07:22 +0900

    fix

--- cia-rana2/main.go
+++ cia-rana2/main.go
@@ -1,9 +1,9 @@
 package main
 
-func main() { a += "\x60"; print(a, a, "\n") }
+func main() { a += "\x60"; println(a + a) }
 
 var a = `package main
 
-func main() { a += "\x60"; print(a, a, "\n") }
+func main() { a += "\x60"; println(a + a) }
 
 var a = `
