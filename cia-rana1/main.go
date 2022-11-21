a0c1b02883a53ec713c78af5e0898d17f5caef84
cia-rana <kiwamura0314@gmail.com>
2022-11-21 22:41:18 +0900

    fix

--- cia-rana1/main.go
+++ cia-rana1/main.go
@@ -3,7 +3,7 @@ package main
 import . "fmt"
 
 func main() {
-	a := "package main\n\nimport . \"fmt\"\n\nfunc main() {\n\ta:=%q\n\tPrintf(a, a)\n}"
+	a := "package main\n\nimport . \"fmt\"\n\nfunc main() {\n\ta := %q\n\tPrintf(a, a)\n}"
 	Printf(a, a)
 }
 
